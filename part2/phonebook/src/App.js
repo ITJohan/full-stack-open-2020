b3a07becc3c3f4c723222d397f96c3946fd2f78b
ITJohan <johanlindkvist89@gmail.com>
2020-05-26 10:35:48 +0200

    Add 2.6 revised

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,19 +4,34 @@ const App = () => {
   const [ persons, setPersons ] = useState([{name: 'Arto Hellas'}])
   const [ newName, setNewName ] = useState('')
 
+  const handleChange = e => {
+    setNewName(e.target.value);
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const newPerson = {
+      name: newName
+    }
+    setPersons([...persons, newPerson])
+    setNewName('')
+  }
+
   return (
     <div>
       <div>Debug: {newName}</div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          Name: <input />
+          Name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
-          <button type='submit'>Add</button>
+          <button type='submit' onClick={handleSubmit}>Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
     </div>
   )
 }
