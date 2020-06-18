11c61b51ea47a5aa66d1439d3c29ba55cc1bc196
ITJohan <johanlindkvist89@gmail.com>
2020-06-18 15:42:49 +0200

    Add 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,11 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((prev, curr) => prev.likes > curr.likes ? prev : curr, {})
+}
+
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, curr) => sum + curr.likes, 0)
 }
 
 module.exports = {
   dummy,
+  favoriteBlog,
   totalLikes
 }
