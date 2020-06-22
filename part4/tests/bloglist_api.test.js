f8257d576ae5e7e26fcb5ba7a552f2a2e2f481eb
ITJohan <johanlindkvist89@gmail.com>
2020-06-22 15:30:23 +0200

    Add 4.9

--- part4/tests/bloglist_api.test.js
+++ part4/tests/bloglist_api.test.js
@@ -19,6 +19,12 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('unique identifier of blogs is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined()
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
