36b0911c10f4c282ca52c427c8229c1aedc7e6b1
ITJohan <johanlindkvist89@gmail.com>
2020-06-30 11:19:35 +0200

    Add 5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const style = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  return (
+    <div style={style}>
+      {blog.title} {blog.author}
+      <button onClick={() => setVisible(!visible)}>{visible ? 'Hide' : 'Show'}</button>
+      {visible && (
+        <div>
+          <div>URL: {blog.url}</div>
+          <div>
+           Likes: {blog.likes}
+            <button>Like</button>
+          </div>
+          <div>User: {blog.user.username}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
