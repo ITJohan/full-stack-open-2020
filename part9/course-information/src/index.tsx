fde79a31608a17cfb025eb556fa5c9c51b5dd199
ITJohan <johanlindkvist89@gmail.com>
2020-09-30 20:52:29 +0200

    add 9.15

--- part9/course-information/src/index.tsx
+++ part9/course-information/src/index.tsx
@@ -1,9 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-interface CoursePart {
-  name: string,
-  exerciseCount: number
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartExtended extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends CoursePartExtended {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartExtended {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+interface CoursePartFour extends CoursePartExtended {
+  name: 'Testing';
+  length: number;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartFour;
+
+const Part: React.FC<{ coursePart: CoursePart }> = ({ coursePart }) => {
+  switch (coursePart.name) {
+    case 'Fundamentals':
+      return (
+        <p>
+          {coursePart.name} {coursePart.exerciseCount} {coursePart.description}
+        </p>
+      );
+    case 'Using props to pass data':
+      return (
+        <p>
+          {coursePart.name} {coursePart.exerciseCount} {coursePart.groupProjectCount}
+        </p>
+      );
+    case 'Deeper type usage':
+      return (
+        <p>
+          {coursePart.name} {coursePart.exerciseCount} {coursePart.description} {coursePart.exerciseSubmissionLink}
+        </p>
+      );
+    case 'Testing':
+      return (
+        <p>
+          {coursePart.name} {coursePart.exerciseCount} {coursePart.description} {coursePart.length}
+        </p>
+      );
+  }
 };
 
 const Header: React.FC<{ name: string }> = ({ name }) => (
@@ -13,9 +67,7 @@ const Header: React.FC<{ name: string }> = ({ name }) => (
 const Content: React.FC<{ courseParts: Array<CoursePart> }> = ({ courseParts }) => (
   <div>
     {courseParts.map((coursePart: CoursePart) => (
-      <p key={coursePart.name}>
-        {coursePart.name} {coursePart.exerciseCount}
-      </p>
+      <Part key={coursePart.name} coursePart={coursePart} />
     ))}
   </div>
 );
@@ -29,18 +81,28 @@ const Total: React.FC<{ courseParts: Array<CoursePart> }> = ({ courseParts }) =>
 
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+    },
+    {
+      name: 'Testing',
+      exerciseCount: 3,
+      description: 'Testing the description',
+      length: 5
     }
   ];
 
