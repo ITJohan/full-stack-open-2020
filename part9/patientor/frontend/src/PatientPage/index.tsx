2f3d5b2e9f5847fb91dbb3666675e56073f7dbda
ITJohan <johanlindkvist89@gmail.com>
2020-10-07 21:33:30 +0200

    Add 9.22

--- part9/patientor/frontend/src/PatientPage/index.tsx
+++ part9/patientor/frontend/src/PatientPage/index.tsx
@@ -2,8 +2,45 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { apiBaseUrl } from "../constants";
-import { Patient } from "../types";
+import { Patient, Entry } from "../types";
 import { useStateValue, setPatient } from "../state";
+
+const EntryDetails: React.FC<{ entry: Entry }> = ({ entry }) => {
+  const style = {
+    border: '1px solid grey',
+    borderRadius: '0.2rem',
+    margin: '1rem 0',
+    padding: '1rem'
+  }
+
+  switch (entry.type) {
+    case 'Hospital':
+      return (
+        <div style={style}>
+          <h3>{entry.date} Hospital</h3>
+          <p>{entry.description}</p>
+          <p>{entry.discharge ? 'Discharged' : 'Not discharged'}</p>
+        </div>
+      );
+    case 'OccupationalHealthcare':
+      return (
+        <div style={style}>
+          <h3>{entry.date} Occupational</h3>
+          <p>{entry.description}</p>
+        </div>
+      );
+    case 'HealthCheck':
+      return (
+        <div style={style}>
+          <h3>{entry.date} Health check</h3>
+          <p>{entry.description}</p>
+          <p>Health rating: {entry.healthCheckRating}</p>
+        </div>
+      );
+    default:
+      return null;
+  }
+}
 
 const PatientPage: React.FC = () => {
   const [{ patients, diagnoses }, dispatch] = useStateValue();
@@ -40,24 +77,7 @@ const PatientPage: React.FC = () => {
       <p>occupation: {patients[id].occupation}</p>
       <h3>Entries</h3>
       {patients[id].entries.map(entry => (
-        <div key={entry.id}>
-          <p>
-            {entry.date} {entry.description}
-          </p>
-          <ul>
-            {entry.diagnosisCodes &&
-            entry.diagnosisCodes.map(code => {
-              const diagnosis = diagnoses
-                .find(d => d.code === code);
-
-              return (
-                <li key={code}>
-                  {code} {diagnosis && diagnosis.name}
-                </li>
-              )
-            })}
-          </ul>
-        </div>
+        <EntryDetails key={entry.id} entry={entry} />
       ))}
     </div>
   )
